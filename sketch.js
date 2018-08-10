08828a02cbc87df2213058397da805512701b758
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-08-10 18:13:55 +0200

    index bg sketch, back button

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+	colorMode(HSB, 255,255,255);
+	createCanvas(windowWidth, windowHeight);
+	background(150);
+	noSmooth();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	background(150);
+	rectMode(CENTER);
+	noStroke();
+	var scl = width / 50;
+	for(var x = -scl+frameCount%scl; x < width; x += scl){
+		for(var y = 0; y < height; y += scl){
+			var d = dist(mouseX, mouseY, x, y);
+			var size = map(d, 0, width, scl, 0);
+			rect(x, y, size, size);
+		}
+	}
+}
