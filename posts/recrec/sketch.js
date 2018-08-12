c1f73aaf43e66c28f89e46ca19229529b1c617d9
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-08-12 16:54:42 +0200

    color

--- posts/recrec/sketch.js
+++ posts/recrec/sketch.js
@@ -10,17 +10,20 @@ function setup() {
         noFill();
         inputX = map(mouseX, width, 0, 1.03,20);
         inputY = map(mouseY, height, 0, 1.03, 1.5);
-        drawRecursively(width/2, height/2, 6, sin(radians(frameCount/6)));
+        drawRecursively(width/2, height/2, .2, sin(radians(frameCount/6)));
     }
 
     function drawRecursively(x , y, size, orientation){
         push();
-        if((size < height+height/3 || size < width+width/3) && size > 5){
-            stroke(0,0,255);
+        if((size < height+height/3 || size < width+width/3) && size > .1){
+						colorMode(HSB);
+            stroke((frameCount/2+size/2)%255,255,255);
             translate(x,y);
             rotate(orientation);
             rect(0,0,size,size);
             pop();
+						strokeWeight(.8);
+
             drawRecursively(x,y, size*inputY, orientation+orientation/inputX);
         }else{
             pop();
