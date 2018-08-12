728c4423d76d73590c933a2600efc8bd146c0e13
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-08-12 15:23:14 +0200

    simplified

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 function setup() {
-	colorMode(HSB, 255,255,255);
 	createCanvas(windowWidth, windowHeight);
-	background(150);
 	noSmooth();
-	rectMode(CENTER);
-	noStroke();
-	fill(255);
-	rect(width/2, height/2, width*.6, height);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+	background(255);
+	noStroke();
+	var scl = width / 50;
+	for(var x = -scl*2; x < width; x+= scl){
+		for(var y = -scl*2; y < height; y+= scl){
+			var d = abs( width/2 - x );
+			fill(map(d, 0, width*.5, 255, 150));
+			rect(x, y, x+scl+1, y+scl+1);
+		}
+	}
 }
