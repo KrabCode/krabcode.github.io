656b064da352e4e4f48ac8d8ad801c456369d136
Jakub Rak <rak@fss.eu>
2018-08-13 14:37:34 +0200

    fixed translate
    rotate now rotates around center of animation, not the corner

--- posts/honeycomb/sketch.js
+++ posts/honeycomb/sketch.js
@@ -14,20 +14,21 @@ function draw() {
 	rotateZ(-PI/8);
 	rotateX(PI/8);
 	rotateY(PI/8);
-	translate(80, 0, -100);
+	
 	strokeWeight(0.9);
 	stroke(255);
 
 	noFill();
 	var greatside = 200;
 	var side = greatside/12;
+	translate(-greatside/2, -greatside/2);
 	for(var x = 0; x < greatside; x+= side){
-		for(var y =0; y < greatside; y+= side){
+		for(var y = 0; y < greatside; y+= side){
 				push();
 				let d = dist(x,y, greatside/2,greatside/2);
 				let a = radians(frameCount/2+d*2);
 				let h = map(sin(a), -1, 1, 0,greatside);
-				translate(x-greatside/2,y-greatside/2);
+				translate(x,y);
 				stroke(map(a%TWO_PI, 0, TWO_PI, 0, 255), 255, 255);
 				box(side, side, h);
 				pop();
