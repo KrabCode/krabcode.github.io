321c318901756aa3ffe04c8a5392c872c6c98b89
Jakub Rak <rak@fss.eu>
2018-08-24 10:37:07 +0200

    cam rotation center fix

--- posts/boxdream/sketch.js
+++ posts/boxdream/sketch.js
@@ -8,9 +8,10 @@ function draw(){
   background(0);
   var w = windowWidth;
   var scl = w/25;  
-  translate(0,0,-windowWidth);
+  
   rotateX(PI/4);
   rotateZ(PI/4);
+  
   for(var x = -w/2; x < w/2; x+= scl){
 	for(var y = -w/2; y < w/2; y+= scl){
       var md  = min(abs(x), abs(y));
@@ -18,7 +19,6 @@ function draw(){
       var td = .5+.5*sin(radians(md-frameCount));
       var zscl = 4*scl*td;
       var h = map(d*td,0,1,0,512)%200;
-
 	  push();
       translate(x,y);
       noStroke();
