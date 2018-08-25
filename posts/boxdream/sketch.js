6a4f06015f14da2f1c4a8542b313360522da02c1
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-08-25 09:20:52 +0200

    mansplaining

--- posts/boxdream/sketch.js
+++ posts/boxdream/sketch.js
@@ -1,17 +1,17 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
 	colorMode(HSB, 255,255,255,255);
-	easycam = new Dw.EasyCam(this._renderer, {distance : windowWidth}); 
+	easycam = new Dw.EasyCam(this._renderer, {distance : windowWidth});
 }
 
 function draw(){
   background(0);
   var w = windowWidth;
-  var scl = w/25;  
-  
+  var scl = w/25;
+
   rotateX(PI/4);
   rotateZ(PI/4);
-  
+
   for(var x = -w/2; x < w/2; x+= scl){
 	for(var y = -w/2; y < w/2; y+= scl){
       var md  = min(abs(x), abs(y));
@@ -19,12 +19,15 @@ function draw(){
       var td = .5+.5*sin(radians(md-frameCount));
       var zscl = 4*scl*td;
       var h = map(d*td,0,1,0,512)%200;
-	  push();
+	  	push();
       translate(x,y);
       noStroke();
       fill(h, 150, 255);
       box(scl, scl, zscl);
-	pop();
+			pop();
 	  }
 	}
+
+	stroke(0, 255, 255);
+	line(200,0,0);
 }
