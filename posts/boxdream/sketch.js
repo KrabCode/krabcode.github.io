b84e76972a6d50270251555bcb8a2a6fdc84a3cc
Jakub Rak <rak@fss.eu>
2018-08-29 10:55:13 +0200

    new and improved palette

--- posts/boxdream/sketch.js
+++ posts/boxdream/sketch.js
@@ -1,30 +1,30 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
 	colorMode(HSB, 255,255,255,255);
-	easycam = new Dw.EasyCam(this._renderer, {distance : windowWidth}); 
+	easycam = new Dw.EasyCam(this._renderer, {distance : windowWidth});
 }
 
 function draw(){
   background(0);
   var w = windowWidth;
-  var scl = w/25;  
-  
+  var scl = w/22;
+
   rotateX(PI/4);
   rotateZ(PI/4);
-  
+
   for(var x = -w/2; x < w/2; x+= scl){
 	for(var y = -w/2; y < w/2; y+= scl){
       var md  = min(abs(x), abs(y));
       var d = map(abs(x)+abs(y), -w, w, 0, 1);
       var td = .5+.5*sin(radians(md-frameCount));
       var zscl = 4*scl*td;
-      var h = map(d*td,0,1,0,512)%200;
-	  push();
+      var h = map(d*td,0,1,0,512)%255;
+	  	push();
       translate(x,y);
       noStroke();
-      fill(h, 150, 255);
+      fill(h, 200, 255);
       box(scl, scl, zscl);
-	pop();
+			pop();
 	  }
 	}
 }
