e6dcb3111b9b412fe0e8d235b07b933c32b1b341
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-09-12 08:23:01 +0200

    lots more scientific

--- posts/earth/sketch.js
+++ posts/earth/sketch.js
@@ -3,9 +3,13 @@ var earthMap;
 var moonMap;
 var stars;
 var starCount = 150;
+var earthRadius = 200; //6,371km
+var scaledKm = earthRadius/6371;
+var distanceDownscale = 10;
+var lunarDistance = scaledKm*400000 / distanceDownscale; //400'000 km
 
 function preload() {
-	moonMap = loadImage("moon.png");
+	moonMap = loadImage("moon.jpg");
 	earthMap = loadImage("earthmap.jpg");
 }
 
@@ -16,18 +20,28 @@ function setup() {
 }
 
 function draw() {
+	var mx = map(mouseX, 0, width, 0, 360);
 	background(0);
+
 	//EARTH
+	push();
 	rotateZ(PI*2);
-	rotateZ(0.401425728);
-	rotateY(-2.1+radians(frameCount/3));
+	//rotateZ(-0.401425728);
+	rotateY(radians(frameCount/3));
 	noStroke();
 	texture(earthMap);
 	ambientLight(25);
-	directionalLight(204, 204, 204, 1, 1, -1);
-	sphere(200);
+	directionalLight(255, 255, 255, 1, 0, 0);
+	sphere(earthRadius);
+	pop();
+	rotateY(radians(frameCount/3/27));
+	rotateZ(radians(-5.14));
 	//MOON
-	translate(850,0,0);
+	translate(lunarDistance,0,0);
+	rotateZ(radians(-6.68));
+	rotateY(1);
 	texture(moonMap);
-	sphere(10);
+	noStroke();
+	sphere(earthRadius*.273);
+
 }
