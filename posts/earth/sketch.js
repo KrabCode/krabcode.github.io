f99e4d4ba68d4c12ff89af45f743760ffc05c2f4
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-09-12 08:28:24 +0200

    welp

--- posts/earth/sketch.js
+++ posts/earth/sketch.js
@@ -3,10 +3,14 @@ var earthMap;
 var moonMap;
 var stars;
 var starCount = 150;
-var earthRadius = 200; //6,371km
+var earthRadius = 250; //6,371km
+
 var scaledKm = earthRadius/6371;
 var distanceDownscale = 10;
 var lunarDistance = scaledKm*400000 / distanceDownscale; //400'000 km
+
+//the sacred texts
+//https://en.wikipedia.org/wiki/Orbit_of_the_Moon#/media/File:Lunar_Orbit_and_Orientation_with_respect_to_the_Ecliptic.tif
 
 function preload() {
 	moonMap = loadImage("moon.jpg");
@@ -20,21 +24,21 @@ function setup() {
 }
 
 function draw() {
+	var earthRotation = frameCount/6;
 	var mx = map(mouseX, 0, width, 0, 360);
 	background(0);
-
 	//EARTH
 	push();
 	rotateZ(PI*2);
 	//rotateZ(-0.401425728);
-	rotateY(radians(frameCount/3));
+	rotateY(radians(earthRotation));
 	noStroke();
 	texture(earthMap);
 	ambientLight(25);
 	directionalLight(255, 255, 255, 1, 0, 0);
 	sphere(earthRadius);
 	pop();
-	rotateY(radians(frameCount/3/27));
+	rotateY(.5+radians(earthRotation/27));
 	rotateZ(radians(-5.14));
 	//MOON
 	translate(lunarDistance,0,0);
