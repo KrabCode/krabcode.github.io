cc20e52277bac6168c1e09fb9be2c6bb237acf5d
Jakub Rak <rak@fss.eu>
2018-10-01 16:10:15 +0200

    styling

--- posts/eternal/sketch.js
+++ posts/eternal/sketch.js
@@ -24,8 +24,8 @@ function drawRect(sizeX, sizeY, gen){
 	 strokeWeight(2);
 	 noFill();
 	 var t = radians(frameCount);
-	 var x = width/2 + width/4*cos(t+gen/20);
-	 var y = height/2 +  height/4*sin(t+gen/20);
+	 var x = width/2 + width/4*cos(t-gen/20);
+	 var y = height/2 +  height/4*sin(t-gen/20);
 	 rect(x,y, sizeX, sizeY);
 	 drawRect(sizeX + 20, sizeY + 20, gen+1);
 }
