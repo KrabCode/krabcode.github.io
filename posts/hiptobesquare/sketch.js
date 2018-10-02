ada731bf103c58f7ff99ad717e84afb61cc9648e
Jakub Rak <rak@fss.eu>
2018-10-02 12:24:00 +0200

    brighter is always in the foreground now

--- posts/hiptobesquare/sketch.js
+++ posts/hiptobesquare/sketch.js
@@ -2,7 +2,6 @@ var cx, cy, side, t, sizeStep, timeStep, genCount, input;
 
 function setup() {
 	createCanvas(800,800);
-	strokeWeight(3);
   noFill();
   rectMode(CENTER);
   colorMode(HSB, 360,100,100,100);
@@ -21,17 +20,19 @@ function draw() {
   side = min(width, height);
   timeStep = map(sin(t/150), -1,1, -PI, PI);
   sizeStep = map(sin(t/50), -1,1, 1, 2);
-  drawRect(sizeStep, sizeStep, 0);
+  drawRect(sizeStep*genCount, sizeStep*genCount, genCount);
 }
 
 function drawRect(sizeX, sizeY, gen){
-   if(gen > genCount){
+   if(gen < 0){
     return;
    }
    var b = 100-map(gen, 0, genCount, 0, 75);
    stroke(0,0,b);
+	 var sw = 3-map(gen, 0, genCount, 0, 2);
+	 strokeWeight(3);
    var x = cx + side/4*cos(t-gen*timeStep);
    var y = cy + side/4*sin(t-gen*timeStep);
    rect(x,y, sizeX, sizeY);
-   drawRect(sizeX + sizeStep, sizeY + sizeStep, gen+1);
+   drawRect(sizeX - sizeStep, sizeY - sizeStep, gen-1);
 }
