5309ee458565d7ae615d77211e5aa1ef406f6fe1
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-10-03 21:11:50 +0200

    calm down!!!

--- posts/galaxy/sketch.js
+++ posts/galaxy/sketch.js
@@ -2,8 +2,8 @@ var maxGen = 400;
 var mx, my;
 
 function setup() {
-	createCanvas(800,800);
-	colorMode(HSB,1,1,1,1);
+	createCanvas(windowWidth,windowHeight);
+	colorMode(RGB,100,100,100);
 	rectMode(CENTER);
 	mx = .99;
 	my = .98;
@@ -13,8 +13,8 @@ function draw() {
 		background(0);
     translate(width/2, height/2);
 		if((mouseX>0 || mouseY>0) && mouseIsPressed){
-			mx = map(mouseX, 0, width, -1, 1);
-			my = map(mouseY, 0, height,-1, 1);
+			mx = map(mouseX, 0, width, -.1, .1);
+			my = map(mouseY, 0, height, -.1, .1);
 		}
     recursive(0);
 }
@@ -24,16 +24,8 @@ function recursive( gen ){
 		var t = .5+.5*frameCount/5000;
 		var x = (maxGen-gen)*(sin(gen*t*mx));
 		var y = (maxGen-gen)*(cos(gen*t*my));
-		stroke(1);
+		stroke(100,100,50+gen/8);
 		strokeWeight(3);
 		point(x,y);
 		recursive(++gen);
-}
-
-function backgroundGradient(){
-    strokeWeight(1);
-    for(var y = 0; y < height; y++){
-        stroke(1-map(y, 0, height, .5, .85));
-        line(0,y,width,y);
-    }
 }
