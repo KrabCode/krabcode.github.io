b93b475a634ccc13084c97efd21b4267eec74702
Jakub Rak <rak@fss.eu>
2018-10-03 10:19:11 +0200

    made header unselectable, improved galaxy on subway

--- posts/galaxy/sketch.js
+++ posts/galaxy/sketch.js
@@ -1,27 +1,31 @@
 var maxGen = 400;
-
+var mx, my;
 
 function setup() {
 	createCanvas(800,800);
 	colorMode(HSB,1,1,1,1);
 	rectMode(CENTER);
+	mx = .5;
+	my = .5;
 }
 
 function draw() {
 		background(0);
     translate(width/2, height/2);
+		if((mouseX>0 || mouseY>0) && mouseIsPressed){
+			mx = map(mouseX, 0, width, 0, 2);
+			my = map(mouseY, 0, height, 0, 2);
+		}
     recursive(0);
 }
 
 function recursive( gen ){
 		if(gen > maxGen) return;
-		var t = frameCount/1000;
-		var m = gen*t;
-		var c = gen/2;
-		var x = (width/4-c)*(sin(t+m));
-		var y = (width/4-c)*(cos(t-m));
+		var t = .5+.5*frameCount/5000;
+		var x = (maxGen-gen)*(sin(gen*t*mx));
+		var y = (maxGen-gen)*(cos(gen*t*my));
 		stroke(1);
-		strokeWeight(1);
+		strokeWeight(5);
 		point(x,y);
 		recursive(++gen);
 }
