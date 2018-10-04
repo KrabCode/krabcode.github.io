429ef21664ddcf00df82ce54af0927f0cb51ef77
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-10-04 10:15:12 +0200

    Update sketch.js

--- posts/galaxy/sketch.js
+++ posts/galaxy/sketch.js
@@ -10,22 +10,22 @@ function setup() {
 }
 
 function draw() {
-		background(0);
-    translate(width/2, height/2);
-		if((mouseX>0 || mouseY>0) && mouseIsPressed){
-			mx = map(mouseX, 0, width, -.5, .5);
-			my = map(mouseY, 0, height, -.5, .5);
-		}
-    recursive(0);
+	background(0);
+    	translate(width/2, height/2);
+	if((mouseX>0 || mouseY>0) && mouseIsPressed){
+		mx = map(mouseX, 0, width, -.5, .5);
+		my = map(mouseY, 0, height, -.5, .5);
+	}
+    	recursive(0);
 }
 
 function recursive( gen ){
-		if(gen > maxGen) return;
-		var t = .5+.5*frameCount/5000;
-		var x = (maxGen-gen)*(sin(gen*t*mx));
-		var y = (maxGen-gen)*(cos(gen*t*my));
-		stroke(50+gen/8);
-		strokeWeight(3);
-		point(x,y);
-		recursive(++gen);
+	if(gen > maxGen) return;
+	var t = .5+.5*frameCount/5000;
+	var x = (maxGen-gen)*(sin(gen*t*mx));
+	var y = (maxGen-gen)*(cos(gen*t*my));
+	stroke(50+gen/8);
+	strokeWeight(3);
+	point(x,y);
+	recursive(++gen);
 }
