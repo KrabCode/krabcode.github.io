04c8536406bb8f4d2e78280ec1b656aa29d97e12
Jakub Rak <rak@fss.eu>
2018-10-05 12:01:20 +0200

    changes

--- posts/pointcloud/sketch.js
+++ posts/pointcloud/sketch.js
@@ -8,7 +8,7 @@ var scl = 10;
 function draw() {
   background(0);
 	input += pmouseX - mouseX;
-	scl = map(mouseY, 0, height, 10, 20);
+	scl = map(mouseY, 0, height, 12, 20);
   translate(width/2, height/2);
   stroke(255);
   fill(255);
@@ -30,7 +30,7 @@ function recursiveRing(i, j, r) {
     return;
   }
   var n = map(i, 0, j, 0, TWO_PI);
-  n+= j*5*sin(radians((input+frameCount)/20));
+  n+= j*5*cos(radians((input+frameCount)/20));
   n = n%TWO_PI;
   var x = r*sin(n);
   var y = r*cos(n);
