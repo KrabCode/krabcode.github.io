bf04b51dddfa6f4f2b99a9949dd37c2c27e95713
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-10-06 16:36:44 +0200

    changed the points to lines yay

--- posts/pointcloud/sketch.js
+++ posts/pointcloud/sketch.js
@@ -8,11 +8,11 @@ var scl = 10;
 function draw() {
   background(0);
 	input += pmouseX - mouseX;
-	scl = map(mouseY, 0, height, 12, 20);
+	scl = map(mouseY, 0, height, 8, 12);
   translate(width/2, height/2);
   stroke(255);
   fill(255);
-  strokeWeight(scl/2);
+  strokeWeight(1);
   recursiveMain(0);
 }
 
@@ -21,7 +21,7 @@ function recursiveMain(i) {
   if (r > min(height,width)/2) {
     return;
   }
-  recursiveRing(0, i*2, r);
+  recursiveRing(0, i, r);
   recursiveMain(++i);
 }
 
@@ -30,10 +30,14 @@ function recursiveRing(i, j, r) {
     return;
   }
   var n = map(i, 0, j, 0, TWO_PI);
-  n+= j*5*cos(radians((input+frameCount)/20));
+  n+= j*10*cos(radians((input+frameCount)/35));
   n = n%TWO_PI;
   var x = r*sin(n);
   var y = r*cos(n);
-  point(x, y);
+	push();
+	translate(x,y);
+	rotate(-n+HALF_PI);
+	line(-scl/2, 0, scl/2, 0);
+	pop();
   recursiveRing(++i, j, r);
 }
