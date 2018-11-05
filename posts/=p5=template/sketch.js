b7e09477ea7f504dea696fe24e0d329a80e476c0
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-11-05 21:10:20 +0100

    mountain

--- posts/=p5=template/sketch.js
+++ posts/=p5=template/sketch.js
@@ -1,23 +1,7 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(.7);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	background(0);
-	pointLight(255,255,255, 500, 0, 0);
-	pointLight(255,255,255, 0, 500,0);
-	pointLight(255,255,255, 0,0, 500);
-
-	rotateX(radians(frameCount/4));
-	rotateY(radians(frameCount/3));
-	rotateZ(radians(frameCount/2));
-
-	stroke(255);
-	ambientMaterial(255);
-	box(150);
+	background(120);
 }
---
+++ posts/mountain/sketch.js
@@ -0,0 +1,73 @@
+
+var res = 125;
+var s = 450;
+var globalCircleAngleOffset;
+var shapes;
+
+
+function setup() {
+	shapes = new Array();
+	globalCircleAngleOffset = PI + HALF_PI / 2;
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB, 360, 100, 100, 100);
+	shapes.push(makeHorizontalLine());
+	shapes.push(makeCircle());
+}
+
+function draw() {
+	background(120);
+}
+
+function settings() {
+		fullScreen(P2D);
+		smooth(8);
+}
+
+function draw() {
+		background(0);
+		var mx = map(mouseX, 0, width, 0, 1);
+		s = windowHeight/2 - windowHeight*mx;
+		print(mx);
+		translate(width / 2, height / 2);
+		rotate(PI);
+		globalCircleAngleOffset = (PI + HALF_PI / 2) + radians(frameCount / 4);
+		shapes[1] = makeCircle();
+		drawShape();
+
+}
+
+function drawShape() {
+		beginShape();
+		for (var i = 0; i < res - 1; i++) {
+				var x0 = shapes[0][i].x;
+				var y0 = shapes[0][i].y;
+				var x1 = shapes[1][i].x;
+				var y1 = shapes[1][i].y;
+
+				strokeWeight(1);
+				stroke(100, 50);
+				line(x0, y0, x1, y1);
+		}
+}
+
+function makeCircle() {
+		var vertices = new Array(res);
+		for (var i = 0; i < res; i++) {
+				var x, y;
+				var a = map(i, 0, res - 3, 0, TWO_PI);
+				a += globalCircleAngleOffset;
+				x = s * cos(a);
+				y = s * sin(a);
+				vertices[i] = createVector(x, y);
+		}
+		return vertices;
+}
+
+function makeHorizontalLine() {
+		var vertices = new Array(res);
+		for (var i = 0; i < vertices.length; i++) {
+				var x = map(i, 0, vertices.length, -s, s);
+				vertices[i] = createVector(x, 0);
+		}
+		return vertices;
+}
